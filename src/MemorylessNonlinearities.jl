1b7eb2d47b0998ca2e568de206a8d7fa95e2e3db
ymtoo <ymtoo86@gmail.com>
2020-10-30 12:50:12 +0800

    feat(*): add approximated sas

--- src/MemorylessNonlinearities.jl
+++ src/MemorylessNonlinearities.jl
@@ -1,11 +1,15 @@
 module MemorylessNonlinearities
 
-using AlphaStableDistributions, QuadGK
+using AlphaStableDistributions, DelimitedFiles, Interpolations, QuadGK
 
 export Blanking, Cauchy, Clipping, HampelThreePart, SαS, TurkeyBiweight
 export filt, minmaxrescale
 
 include("utils.jl")
+
+const _xsas = -200:0.1:200
+const _αsas = 1.0:0.001:2.0
+const _ysas = readdlm(joinpath(@__DIR__, "..", "data/sas.csv"), ',', Float64)
 
 abstract type AbstractMemorylessNonlinearity end
 
@@ -31,8 +35,10 @@ struct SαS{T<:Real} <: AbstractMemorylessNonlinearity
     α::T
     scale::T
     location::T
+    approx::Bool
 end
-SαS(α) = SαS(α, 1.0, 0.0)
+SαS(α, scale, location) = SαS(α, scale, location, false)
+SαS(α) = SαS(α, 1.0, 0.0, false)
 
 struct TurkeyBiweight{T<:Real} <: AbstractMemorylessNonlinearity
     k::T
@@ -88,12 +94,34 @@ Symmetric Alpha Stable nonlinearity.
 `x` is standard symmetric alpha stable distributed.
 """
 function sαs(x::S, α) where {S<:Real}
-    num, _ = quadgk(t -> t * sin(t * x) * exp(-t^α), 0, Inf; rtol=1e-6, atol=1e-9) 
-    den, _ = quadgk(t -> cos(t * x) * exp(-t^α), 0, Inf; rtol=1e-6, atol=1e-9)
+    num, _ = quadgk(t -> t * sin(t * x) * exp(-t^α), 0, Inf; atol=1e-12, order=3) 
+    den, _ = quadgk(t -> cos(t * x) * exp(-t^α), 0, Inf; atol=1e-12, order=3)
     num / den
 end
+K(α) = 13.0859 * α^4 - 68.4388 * α^3 + 134.7758 * α^2 - 115.9855 * α + 37.6752
+function sαs(x::S, Kα, k) where {S<:Real} 
+    τ = √(Kα/k)
+    (abs(x) > τ) ? Kα / x : k*x
+end
+
 function filt(f::SαS, x::AbstractVector)
-    sαs.((x .- f.location) ./ f.scale, f.α)
+    (abs(f.α - 2.0) < 0.001) && return (x ./ 2) # Gaussian
+    (abs(f.α - 1.0) < 0.001) && return (2 .* x) ./ (1 .+ x.^2) # Cauchy 
+    xstd = (collect(x) .- f.location) ./ f.scale
+    if f.approx
+        # k = gamma(3/f.α) / gamma(1/f.α)
+        # sαs.(xstd, Kα, k)
+        maxval = maximum(_xsas)
+        minval = minimum(_xsas)
+        Kα = K(f.α)
+        xstd[xstd .> maxval] .= (f.α+1) ./ xstd[xstd .> maxval]#Kα ./ xstd[xstd .> maxval]
+        xstd[xstd .< minval] .= (f.α+1) ./ xstd[xstd .< minval]#Kα ./ xstd[xstd .< minval]
+        itp = interpolate((_αsas, _xsas), _ysas, Gridded(Linear()))
+        xstd[(xstd .<= maxval) .& (xstd .>= minval)] .= itp(f.α, xstd[(xstd .<= maxval) .& (xstd .>= minval)])
+        xstd
+    else
+        sαs.(xstd, f.α)
+    end
 end
 
 """
