7140639b185e1b7873c350cd1820807fb3af084d
ymtoo <ymtoo86@gmail.com>
2020-11-06 23:21:47 +0800

    feat(*): add smoothing

--- perf/simulate.jl
+++ perf/simulate.jl
@@ -1,15 +1,16 @@
 using AlphaStableDistributions
 using Distributions
 using DSP
+using ImageFiltering
 using MemorylessNonlinearities
 using SignalAnalysis
 using StatsBase
 
 export simulate
 
-const _α = 1.5
-const _scale = 1.
-const _location = 0.
+# const _α = 1.5
+# const _scale = 1.
+# const _location = 0.
 
 function rand_chirp(fs, recordtlen, chirptlen, freqs::Tuple)
     N = trunc(Integer, fs * recordtlen)
@@ -36,20 +37,15 @@ function simulate(gs=nothing;
                   recordtlen=1, 
                   chirptlen=1, 
                   freqs=(100, 2000),
-                  fs=9600)
-    # (gs === nothing) && (gs = [(Blanking, 3 * _scale), 
-    #                            (CauchyNL, 3 * _scale), 
-    #                            (Clipping, 3 * _scale),
-    #                            (HampelThreePart, 3 * _scale, 6 * _scale, 9 * _scale),
-    #                            (SαSNL, _α, 3 * _scale, _location),
-    #                            (TurkeyBiweight, 3 * _scale)])
+                  fs=96000,
+                  smoothen=nothing)
     ngsnrs = length(gsnrs)
     ngs = gs === nothing ? length(nlnames()) : length(gs)
     E = zeros(nrealizations, ngsnrs, ngs)
     for i in 1:ngsnrs
         for j in 1:nrealizations
             s = rand_chirp(fs, recordtlen, chirptlen, freqs)
-            v = rand(AlphaStable(α=_α, scale=_scale, location=_location), length(s))
+            v = rand(AlphaSubGaussian(n=length(s)))
             atten = get_attenuation(s, v, gsnrs[i])
             x = atten .* s .+ v
             σ = mad(x)
@@ -62,6 +58,7 @@ function simulate(gs=nothing;
                                  (TurkeyBiweight, 3 * σ)])
             for (k, g) in enumerate(gs)
                 ŝ = MemorylessNonlinearities.filt(g[1](g[2:end]...), x)
+                (smoothen !== nothing) && (ŝ = mapwindow(smoothen, ŝ, (11,)))
                 a = (ŝ's) / (ŝ'ŝ)
                 E[j, i, k] = rms(s - a .* ŝ)
             end
